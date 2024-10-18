5e183e2252f377ec58bc7e83194e0f58520c9a07
Hessam Ghassemi <s.hesam.ghasemi@gmail.com>
2024-10-18 18:47:08 +0330

    remove prints from 1-valid-anagram.py

--- 1-valid-anagram.py
+++ 1-valid-anagram.py
@@ -9,14 +9,12 @@ class Solution:
                 number_of_visited_letters[c] += 1
                 continue
             number_of_visited_letters[c] = 1
-        print(number_of_visited_letters)
         for c in t:
             if c not in number_of_visited_letters:
                 return False
             if number_of_visited_letters[c] == 0:
                 return False
             number_of_visited_letters[c] -= 1
-        print(number_of_visited_letters)
         all_zero = all(x == 0 for x in number_of_visited_letters.values())
         if all_zero:
             return True
