bd447e4a1df6421eacd0717fa8c004d0158a489f
Hessam Ghassemi <s.hesam.ghasemi@gmail.com>
2025-01-03 14:48:21 +0330

    return list instead of tuple

--- 023-two-sum-ii-input-array-is-sorted.py
+++ 023-two-sum-ii-input-array-is-sorted.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         l, r = 0 , len(numbers) - 1
@@ -5,7 +8,7 @@ class Solution:
         while l < r:
             s = numbers[l] + numbers[r]
             if target == s:
-                return l + 1, r + 1
+                return [l + 1, r + 1]
             elif target < s:
                 r -= 1
             else:
