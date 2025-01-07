22b34f25df3cf8da651201a5f9e29d498c501e6a
Hessam Ghassemi <s.hesam.ghasemi@gmail.com>
2025-01-07 15:54:11 +0330

    clean

--- 008-longest-common-prefix.py
+++ 008-longest-common-prefix.py
@@ -1,12 +1,15 @@
 # https://leetcode.com/problems/longest-common-prefix/
 
+from collections import deque
+from typing import List
+
 TEST_CASES = [
     ({"strs": ["flower","flow","flight"]}, "fl"),
-    ({"strs": ["flower","flow","flight"]}, ""),
+    ({"strs": ["dog","racecar","car"]}, ""),
 ]
 
 
-class Solution:
+class Solution1:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         prefix = strs[0]
         if len(strs)==1:
@@ -19,8 +22,24 @@ class Solution:
         return prefix
 
 
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+
+        first, others = strs[0], strs[1:]
+        prefix = first
+        for w in others:
+            i = 0
+            while i<len(prefix) and i<len(w) and prefix[i] == w[i]:
+                i += 1
+            
+            prefix = prefix[:i]
+        return prefix
+
+
 if __name__ == "__main__":
-    for inputs,expected_output in test_cases:
+    for inputs,expected_output in TEST_CASES:
         actual_output = Solution().longestCommonPrefix(**inputs)
         assert actual_output == expected_output, f"{inputs=}, {actual_output=}, {expected_output=}"
 
