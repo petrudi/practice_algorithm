a7272226e6191fd74d75e1be0df451fe879dee00
Hessam Ghassemi <s.hesam.ghasemi@gmail.com>
2025-01-10 21:50:05 +0330

    add 031-word-pattern.py

---
+++ 031-word-pattern.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/word-pattern/
+
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        ht1 = {}
+        ht2 = {}
+        s = s.split()
+        if len(pattern)!=len(s):
+            return False
+        
+        p = 0
+        while p < len(pattern):
+            if pattern[p] in ht1 and ht1[pattern[p]] != s[p]:
+                return False
+            ht1[pattern[p]] = s[p]
+            if s[p] in ht2 and ht2[s[p]] != pattern[p]:
+                return False
+            ht2[s[p]] = pattern[p]
+            p += 1
+        return True
