4c6d12fbe9f3374345eb73fc3d7815b31ae54eec
Hesam Ghasemi <s.hesam.ghasemi@gmail.com>
2025-05-25 22:14:22 +0330

    Fix testcase for 001-valid-anagram.py

--- 001-valid-anagram.py
+++ 001-valid-anagram.py
@@ -10,18 +10,17 @@ class Solution:
             if c not in counter or counter[c] == 0:
                 return False
             counter[c] -= 1
-        all_zero = all(x == 0 for x in counter.values())
-        if all_zero:
-            return True
-        return False
+        all_zeros = all(x == 0 for x in counter.values())
+        return all_zeros
+
 
 if __name__ == "__main__":
     test_cases = [
         ({"s": "anagram","t": "nagaram"}, True),
-        ({"s": "rat","t": "car"}, True),
+        ({"s": "rat","t": "car"}, False),
     ]
     for inputs,expected_output in test_cases:
         actual_output = Solution().isAnagram(**inputs)
-        assert actual_output == expected_output
+        assert actual_output == expected_output, f"{actual_output=}, {expected_output=}"
 
     print("all passed!")
