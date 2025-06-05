7fec3b4a7ea4fd4dc8b601d731249366bf636ea2
Hesam Ghasemi <44605128+petrudi@users.noreply.github.com>
2025-06-05 09:59:31 +0330

    Update s003_valid_palindrome.py

--- solutions/s003_valid_palindrome.py
+++ solutions/s003_valid_palindrome.py
@@ -8,14 +8,11 @@ class Solution:
         only_alphanumeric: str = re.sub(r"[^a-zA-Z0-9]", "", s.lower())
         l = 0
         r = len(only_alphanumeric) - 1
-        for _ in range(len(only_alphanumeric)):
+        while l<r:
             if only_alphanumeric[l] != only_alphanumeric[r]:
                 return False
             l += 1
             r -= 1
-
-            if l >= r:
-                return True
 
         return True
 
