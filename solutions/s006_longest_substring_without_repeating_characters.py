f89f348839b101bd5711a6a47350bdbc5e5f54d1
Hesam Ghasemi <44605128+petrudi@users.noreply.github.com>
2025-06-05 10:44:45 +0330

    Update s006_longest_substring_without_repeating_characters.py

--- solutions/s006_longest_substring_without_repeating_characters.py
+++ solutions/s006_longest_substring_without_repeating_characters.py
@@ -5,17 +5,16 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         l = r = 0
         m = 0
-        mem = set()
+        mem:dict[str,int] = {} #item,index
         while r < len(s):
             # validate and clean substring
             # calculate max
             # update set
-            while s[r] in mem:
-                mem.remove(s[l])
-                l += 1
+            if s[r] in mem:
+                l = mem[s[r]] + 1
             w = r - l + 1
             m = max(m, w)
-            mem.add(s[r])
+            m[s[r]] = r
             r += 1
 
         return m
