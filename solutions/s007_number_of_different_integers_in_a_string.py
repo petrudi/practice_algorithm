da286419fcc5308687d8db1178413d63802a0d20
Hesam Ghasemi <44605128+petrudi@users.noreply.github.com>
2025-06-05 10:55:05 +0330

    Update s007_number_of_different_integers_in_a_string.py

--- solutions/s007_number_of_different_integers_in_a_string.py
+++ solutions/s007_number_of_different_integers_in_a_string.py
@@ -8,17 +8,3 @@ class Solution:
         word = set([int(w) for w in word])
         return len(word)
 
-
-if __name__ == "__main__":
-    test_cases = [
-        ({"word": "a123bc34d8ef34"}, 3),
-        ({"word": "leet1234code234"}, 2),
-        ({"word": "a1b01c001"}, 1),
-    ]
-    for inputs, expected_output in test_cases:
-        actual_output = Solution().numDifferentIntegers(**inputs)
-        assert actual_output == expected_output, (
-            f"{inputs=}, {actual_output=}, {expected_output=}"
-        )
-
-    print("all passed!")
