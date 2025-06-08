766777a03221b4904cc30bdb4f6352bdcc3852f0
Hesam Ghasemi <s.hesam.ghasemi@gmail.com>
2025-06-08 09:45:11 +0330

    clean solutions/s008_longest_common_prefix.py

--- solutions/s008_longest_common_prefix.py
+++ solutions/s008_longest_common_prefix.py
@@ -3,32 +3,14 @@
 from typing import List
 
 
-class Solution1:
+class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         prefix = strs[0]
-        if len(strs) == 1:
-            return prefix
 
         for s in strs[1:]:
             while not s.startswith(prefix):
-                prefix = prefix[:-1]
+                prefix = prefix[:-1] # remove last character
+                if not prefix:
+                    return ""
 
         return prefix
-
-
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs) == 1:
-            return strs[0]
-
-        first, others = strs[0], strs[1:]
-        prefix = first
-        for w in others:
-            i = 0
-            while i < len(prefix) and i < len(w) and prefix[i] == w[i]:
-                i += 1
-
-            prefix = prefix[:i]
-        return prefix
-
-
